ab2cf0d379b9163d9daee00f273fb9a67348ca80
lgustavocampos <lgustavocampos@gmail.com>
2018-08-27 22:55:38 -0300

    Initial Commit

---
+++ ws-pwddigest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ws-pwddigest
+//
+//  Created by GUSTAVO CAMPOS on 27/08/18.
+//  Copyright © 2018 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
